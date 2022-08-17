3b155616e2f773218d78f204036d482840e1e30b
Gabriela Rezende <gabriela.mrezende@outlook.com.br>
2022-08-17 09:56:05 -0300

    padrão DTO para Tecnico

--- src/main/java/com/gabriela/helpdesk/resources/TecnicoResourse.java
+++ src/main/java/com/gabriela/helpdesk/resources/TecnicoResourse.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gabriela.helpdesk.domain.Tecnico;
+import com.gabriela.helpdesk.domain.dtos.TecnicoDTO;
 import com.gabriela.helpdesk.services.TecnicoService;
 
 @RestController
@@ -18,8 +19,8 @@ public class TecnicoResourse {
 	private TecnicoService service;
 	
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id) {
 		Tecnico obj = service.findById(id);
-		return ResponseEntity.ok().body(obj);
+		return ResponseEntity.ok().body(new TecnicoDTO(obj));
 	}
 }
