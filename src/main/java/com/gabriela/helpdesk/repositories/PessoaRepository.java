1d383ed4804a3f3ed291743e1f860472a1dfdf7d
Gabriela Rezende <gabriela.mrezende@outlook.com.br>
2022-08-21 15:46:57 -0300

    exceção para CPF e e-mail ja cadastrados

--- src/main/java/com/gabriela/helpdesk/repositories/PessoaRepository.java
+++ src/main/java/com/gabriela/helpdesk/repositories/PessoaRepository.java
@@ -1,9 +1,13 @@
 package com.gabriela.helpdesk.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.gabriela.helpdesk.domain.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer> {
 	
+	Optional<Pessoa> findByCpf(String cpf);
+	Optional<Pessoa> findByEmail(String email);
 }
